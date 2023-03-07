65fc82fda32d30fd7584cbf50f7ea1d7ff540db1
adaobraga <adaobraga@creago.org.br>
2023-03-07 16:01:59 -0300

    up!

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -15,8 +15,14 @@ defined('BASEPATH') or exit('No direct script access allowed');
 		<h1>Welcome API feita em CodeIgniter!</h1>
 
 		<div id="body">
-			<p>Acessos</p>
+			<br><br>
+			<hr>
+			<h3>App AngularJS</h3>
+			<a href="http://localhost/sisgestorv2/app/">App#!</a>
 			<br>
+			<br>
+			<hr>
+			<pre>API REST</pre>
 			<p>GET</p><code>http://localhost/sisgestorv2/contato</code>
 			<p>GET Single</p><code>http://localhost/sisgestorv2/contato/{num}</code>
 			<p>POST(CREATE)</p><code>http://localhost/sisgestorv2/contato/create</code>
@@ -24,10 +30,11 @@ defined('BASEPATH') or exit('No direct script access allowed');
 			<p>DELETE</p><code>http://localhost/sisgestorv2/contato/delete/{num}</code>
 		</div>
 
-		<p style="text-align: center; margin-top: 12%;">Render <strong>{elapsed_time}</strong> seconds. <?php echo (ENVIRONMENT === 'development') ?  'CI Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
-		<p style="text-align: center; margin-top: 1%;"><strong>By</strong> Adão Braga.</p>
 	</div>
-
+	<footer>
+		<p style="text-align: center; margin-top: 12%;">Render <strong>{elapsed_time}</strong> seconds. <?php echo (ENVIRONMENT === 'development') ?  'CI Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+		<p style="text-align: center; margin-top: 1%;"> Copyright &copy; 2023 - <?php echo date("Y") ?> <strong>By</strong> Adão Braga!</p>
+	</footer>
 </body>
 
 </html>
