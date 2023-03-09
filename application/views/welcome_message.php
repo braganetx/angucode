d878ad4e2c00a5abdc559c2687a8019ac9db02c4
adaobraga <adaobraga@creago.org.br>
2023-03-09 11:40:09 -0300

    UP!

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -22,12 +22,31 @@ defined('BASEPATH') or exit('No direct script access allowed');
 			<br>
 			<br>
 			<hr>
-			<pre>API REST</pre>
+			<p><b>PESSOA</b></p>
 			<p>GET</p><code>http://localhost/sisgestorv2/contato</code>
 			<p>GET Single</p><code>http://localhost/sisgestorv2/contato/{num}</code>
 			<p>POST(CREATE)</p><code>http://localhost/sisgestorv2/contato/create</code>
 			<p>PUT(EDIT)</p><code>http://localhost/sisgestorv2/contato/update/{num}</code>
 			<p>DELETE</p><code>http://localhost/sisgestorv2/contato/delete/{num}</code>
+			<br><br>
+			<hr>
+			<p><b>OPERADORAS</b></p>
+			<p>GET</p><code>http://localhost/sisgestorv2/operadora</code>
+			<p>GET Single</p><code>http://localhost/sisgestorv2/operadora/{num}</code>
+			<p>POST(CREATE)</p><code>http://localhost/sisgestorv2/operadora/create</code>
+			<p>PUT(EDIT)</p><code>http://localhost/sisgestorv2/operadora/update/{num}</code>
+			<p>DELETE</p><code>http://localhost/sisgestorv2/operadora/delete/{num}</code>
+			<br><br>
+			<hr>
+			<p><b>CORES</b></p>
+			<p>GET</p><code>http://localhost/sisgestorv2/cores</code>
+			<p>GET Single</p><code>http://localhost/sisgestorv2/cores/{num}</code>
+			<p>POST(CREATE)</p><code>http://localhost/sisgestorv2/cores/create</code>
+			<p>PUT(EDIT)</p><code>http://localhost/sisgestorv2/cores/update/{num}</code>
+			<p>DELETE</p><code>http://localhost/sisgestorv2/cores/delete/{num}</code>
+			<br>
+			<br>
+			<hr>
 		</div>
 
 	</div>
